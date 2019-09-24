4be90965cd67383b09b8aa9502d01262a9bf877b
Jackd56 <1129694@senati.pe>
2019-09-24 08:28:41 -0500

    Se cambio un nombre

--- AgregarCronograma.php
+++ AgregarCronograma.php
@@ -44,7 +44,7 @@ require'funciones.php';
     <link rel="icon" type="image/png" href="assets/img/favicon.png">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
-    <title>GESTIÓN DE TECNICOS</title>
+    <title>GESTIÓN DE CRONOGRAMA</title>
 
     <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
     <meta name="viewport" content="width=device-width" />
